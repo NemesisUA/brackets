3c059eae22c9671c73ba317ec12cfb5bfeadeba6
NemesisUA <display.n0ne.01@gmail.com>
2020-10-17 17:28:37 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBracket = ['(','[','{','|','1','3','5','7','8'];
+  const closeBracket = [')',']','}','|','2','4','6','7','8'];
+  let strArr = str.split('');
+  // if str[i] = openBracket[j] AND str[i+1] = closeBracket[j] then we can simplifier that sentense, by deleting str[i] and str[i+1]
+  //let simplifiedStr = str;
+  
+  for (i = 0; i < strArr.length; i++) {
+    let typeOfBrackets = openBracket.indexOf(strArr[i]);
+    
+    // if opening bracket is followed by the same clothing brackets - delate them
+    if ((closeBracket.indexOf(strArr[i + 1]) === typeOfBrackets) && (typeOfBrackets !== -1)) {
+      strArr.splice(i, 2);
+      i = -1; // start cicle again      
+    }
+
+  }  
+  if (strArr.length === 0) return true;
+    else return false;
 }
